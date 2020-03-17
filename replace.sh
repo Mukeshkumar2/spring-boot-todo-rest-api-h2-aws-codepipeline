ac5129328ddaa3d57cdb53a01e37f074bf3f4358
Mukeshkumar2 <59080342+Mukeshkumar2@users.noreply.github.com>
2020-03-17 13:34:18 +0530

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,2 +1,2 @@
-aws s3 --region us-east-2 cp s3://venusamplebucket/shell_scripts/jar.sh /var/www/html/
+aws s3 --region us-east-1 cp s3://mkpbucket/shell_scripts/jar.sh /var/www/html/
 sh /var/www/html/jar.sh
